1c85e6c181510ce8610278b637cf184902c10770
bparida <bismayaparida02@gmail.com>
2025-05-15 11:52:08 +0530

    Update LoginPage object with new flow

--- pages/login_page.py
+++ pages/login_page.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 class LoginPage:
     def __init__(self, driver):
         self.driver = driver
-
+#login 
     def login_as_user(self, username, password):
         self.driver.find_element(By.ID, "username").send_keys(username)
         self.driver.find_element(By.ID, "password").send_keys(password)
